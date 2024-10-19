f7b613ba23a3449e8dd4665dd904e49bd61e1b36
saigon math <saigonmath@gmail.com>
2024-10-19 15:05:16 +0700

    add one more api

--- identity-service/src/main/java/com/softz/identity_service/controller/UserController.java
+++ identity-service/src/main/java/com/softz/identity_service/controller/UserController.java
@@ -49,4 +49,9 @@ public class UserController {
     public UserDto getUserByUName(@Param("username") String username){
         return userService.getUserByUsername(username);
     }
+
+    @GetMapping("/users/filters")
+    public UserDto getUserByUNamePassword(@Param("username") String username){
+        return userService.getUserByUsername(username);
+    }
 }
